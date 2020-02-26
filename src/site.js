1308f5b105d1e5c77f99876ede0ab9340974757c
Rui Ramos <rui@ometria.com>
2020-02-26 09:40:25 +0000

    Fix rings, prettier formatting

--- src/site.js
+++ src/site.js
@@ -1,19 +1,18 @@
-const fs = require("fs");
-require("./common");
-require("./images/logo.png");
-require("./images/radar_legend.png");
-const marked = require("marked");
+require('./common');
+require('./images/logo.png');
+require('./images/radar_legend.png');
+const marked = require('marked');
 
 const cache = {};
 function importAll(r) {
-  r.keys().forEach(key => (cache[key] = r(key)));
+  r.keys().forEach((key) => (cache[key] = r(key)));
 }
 
-importAll(require.context("../data/", true, /\.md$/));
+importAll(require.context('../data/', true, /\.md$/));
 
 const knownQuadrants = [];
 const quadrants = {};
-Object.keys(cache).forEach(folder => {
+Object.keys(cache).forEach((folder) => {
   const key = folder.match(/^\.\/(.+)\//)[1];
 
   if (knownQuadrants.includes(key)) return;
@@ -23,7 +22,7 @@ Object.keys(cache).forEach(folder => {
 
   let label;
   if (cache[labelKey]) {
-    label = cache[labelKey].replace("# ", "").trim();
+    label = cache[labelKey].replace('# ', '').trim();
   } else {
     label = key;
   }
@@ -31,32 +30,32 @@ Object.keys(cache).forEach(folder => {
   quadrants[key] = label;
 });
 
-const columns = "name,ring,quadrant,isNew,description";
+const columns = 'name,ring,quadrant,isNew,description';
 let output = [columns];
-Object.keys(cache).forEach(file => {
+Object.keys(cache).forEach((file) => {
   try {
     const fnMatch = file.match(/^\.\/(.+)\/(.+)\.md$/);
-    if (!fnMatch || fnMatch[2] === "index") return;
+    if (!fnMatch || fnMatch[2] === 'index') return;
 
-    const ring = fnMatch[1].split("-")[0];
+    const ring = fnMatch[2].split('-')[0];
     const quadrant = quadrants[fnMatch[1]];
 
-    const contents = cache[file].split("\n").filter(line => line.length);
+    const contents = cache[file].split('\n').filter((line) => line.length);
 
     const name = contents
       .shift()
-      .replace("# ", "")
+      .replace('# ', '')
       .trim();
 
     let isNew;
-    if (contents[0].toUpperCase() === "NEW") {
+    if (contents[0].toUpperCase() === 'NEW') {
       isNew = true;
       contents.shift();
     } else {
       isNew = false;
     }
 
-    const desc = marked.inlineLexer(contents.join("").trim(), []);
+    const desc = marked.inlineLexer(contents.join('').trim(), []);
 
     output.push(`${name},${ring},${quadrant},${isNew},${desc}`);
   } catch (e) {
@@ -65,8 +64,8 @@ Object.keys(cache).forEach(file => {
   }
 });
 
-output = output.join("\n");
-const Radar = require("./util/factory");
+output = output.join('\n');
+const Radar = require('./util/factory');
 
-Radar().buildFromString(output, "Ometria Tech Radar");
+Radar().buildFromString(output, 'Ometria Tech Radar');
 console.log(`This is the CSV generated from the data:\n${output}`);
